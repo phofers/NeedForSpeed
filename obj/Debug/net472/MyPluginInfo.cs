ec7e81bb06aa88484944bf700d1b20765a8413d3
phofers <adamkuc915@gmail.com>
2025-07-09 22:20:25 -0500

    first commit

---
+++ obj/Debug/net472/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace NeedForSpeed
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "NeedForSpeed";
+        public const string PLUGIN_NAME = "NeedForSpeed";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
