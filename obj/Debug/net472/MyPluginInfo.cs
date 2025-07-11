76c6937c1c6bfd1a038feb1aab08658d8228e919
phofers <adamkuc915@gmail.com>
2025-07-11 13:02:49 -0500

    bump version and upgrade GameLibs version

--- obj/Debug/net472/MyPluginInfo.cs
+++ obj/Debug/net472/MyPluginInfo.cs
@@ -4,6 +4,6 @@ namespace NeedForSpeed
     {
         public const string PLUGIN_GUID = "NeedForSpeed";
         public const string PLUGIN_NAME = "NeedForSpeed";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.1.0";
     }
 }
